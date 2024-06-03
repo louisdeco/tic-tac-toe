255e31e867c754f1823c2e6cea138547f5bb31e7
luchodeco <72599025+louisdeco@users.noreply.github.com>
2024-06-03 16:16:47 +0400

    Add gameBoard module
    Allow the creation of the gameBoard, the ability
    to return it, and to drop a token inside a cell.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,19 @@
-console.log("hello")
+const gameBoard = (function () {
+    const columns_rows = 3;
+    const board = [];
+    for (let i = 0; i < columns_rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns_rows; j++) {
+            board[i].push(0);
+        }
+    }
+
+    const getBoard = () => board;
+
+    const dropToken = (row, column) => {
+        if (board[row][column] !== 0) return;
+        board[row][column] = 1;
+    }
+
+    return {getBoard, dropToken, printBoard};
+})();
