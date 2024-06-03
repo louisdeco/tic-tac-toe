c6ab7959f7187f891b40883055201f5f08e6485f
luchodeco <72599025+louisdeco@users.noreply.github.com>
2024-06-03 16:35:43 +0400

    Create cell factory function
    Allow to populate the gameBoard arrays with Cells
    object. The object allow the deposal of a player
    token and to get the value inside it.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,19 +1,35 @@
+const player1 = 1;
+
 const gameBoard = (function () {
     const columns_rows = 3;
     const board = [];
     for (let i = 0; i < columns_rows; i++) {
         board[i] = [];
         for (let j = 0; j < columns_rows; j++) {
-            board[i].push(0);
+            board[i].push(Cell());
         }
     }
 
     const getBoard = () => board;
 
     const dropToken = (row, column) => {
-        if (board[row][column] !== 0) return;
-        board[row][column] = 1;
+        if (board[row][column] < 1) return;
+        board[row][column].addToken(player1);
+        alert(board[row][column].getValue());
     }
 
-    return {getBoard, dropToken, printBoard};
+    return {getBoard, dropToken};
 })();
+
+
+function Cell() {
+    let value = 0;
+
+    const addToken = (player) => {
+        value = player;
+    }
+
+    const getValue = () => value;
+
+    return {addToken, getValue};
+}
